625b97c335a520a0127cb2b1239018a243f76f3c
Daniel Berg <mail@roosta.sh>
2021-05-29 18:02:00 +0200

    Impl distance for Color struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate image;
+use std::collections::HashMap;
 use image::Rgb;
 
 mod palette;
-use palette::Palette;
+use palette::Color;
 
 fn main() {
     let palette = match palette::read_palette() {
@@ -10,27 +11,17 @@ fn main() {
         Err(e) => panic!("Failed to read palette file\n {}", e),
     };
 
-
+    // println!("{:?}", palette.get("black").unwrap());
     convert(&palette);
 }
 
-fn distance(rgb1: &Vec<u8>, rgb2: &Rgb<u8>) -> f32 {
-    let (r1, g1, b1) = (rgb1[0] as f32, rgb1[1] as f32, rgb1[2] as f32);
-    let (r2, g2, b2) = (rgb2[0] as f32, rgb2[1] as f32, rgb2[2] as f32);
-    (((r2 - r1) * 0.30).powi(2) + ((g2 - g1) * 0.59).powi(2) + ((b2 - b1) * 0.11).powi(2)).sqrt()
-}
-
-fn convert(palette: &Palette) {
+fn convert(palette: &HashMap<String, Color>) {
     let mut img = image::open("example.png")
         .expect("Failed to read input image")
         .to_rgb8();
 
-    // let new = &img
-    //     .enumerate_pixels()
-    //     .map(|(x, y, px)|)
-
     for (x, y, px) in img.enumerate_pixels() {
-        let d = distance(&palette.green.rgb, &px);
+        let d = &palette.get("green").unwrap().distance(&px);
         println!("{}", d);
     }
 }
--- src/palette.rs
+++ src/palette.rs
@@ -2,36 +2,8 @@ use std::io::Error;
 use std::fs::File;
 use std::io::Read;
 use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub struct Palette {
-    pub black: Color,
-    pub red: Color,
-    pub green: Color,
-    pub yellow: Color,
-    pub blue: Color,
-    pub magenta: Color,
-    pub cyan: Color,
-    pub white: Color,
-    pub orange: Color,
-    pub bright_black: Color,
-    pub bright_red: Color,
-    pub bright_green: Color,
-    pub bright_yellow: Color,
-    pub bright_blue: Color,
-    pub bright_magenta: Color,
-    pub bright_cyan: Color,
-    pub bright_white: Color,
-    pub bright_orange: Color,
-    pub teal: Color,
-    pub hard_black: Color,
-    pub xgray1: Color,
-    pub xgray2: Color,
-    pub xgray3: Color,
-    pub xgray4: Color,
-    pub xgray5: Color,
-    pub xgray6: Color
-}
+use std::collections::HashMap;
+use image::Rgb;
 
 #[derive(Debug, Deserialize)]
 pub struct Color {
@@ -40,11 +12,19 @@ pub struct Color {
     pub hsl: Vec<u32>
 }
 
-pub fn read_palette() -> Result<Palette, Error> {
+impl Color {
+    pub fn distance(&self, other: &Rgb<u8>) -> f32 {
+        let (r1, g1, b1) = (self.rgb[0] as f32, self.rgb[1] as f32, self.rgb[2] as f32);
+        let (r2, g2, b2) = (other[0] as f32, other[1] as f32, other[2] as f32);
+        (((r2 - r1) * 0.30).powi(2) + ((g2 - g1) * 0.59).powi(2) + ((b2 - b1) * 0.11).powi(2)).sqrt()
+    }
+}
+
+pub fn read_palette() -> Result<HashMap<String, Color>, Error> {
     let mut file = File::open("palette.toml")?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
-    let palette: Palette = toml::from_str(&buffer)?;
+    let palette: HashMap<String, Color> = toml::from_str(&buffer)?;
     Ok(palette)
 }
 
