5639a826ab9da743475c4c112d2b586ff50fcedb
Daniel Berg <mail@roosta.sh>
2021-06-01 13:11:07 +0200

    Use clap for input/output args

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,37 @@ use image::Rgb;
 mod palette;
 use palette::Color;
 
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
     let palette = match palette::read_palette() {
         Ok(palette) => palette,
         Err(e) => panic!("Failed to read palette file\n {}", e),
     };
-    convert(&palette);
+
+    let matches = App::new("srcery-convert")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::new("INPUT")
+            .about("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .arg(Arg::new("OUTPUT")
+            .about("Sets the output file")
+            .required(true)
+            .index(2))
+        .get_matches();
+
+    let input_file = matches.value_of("INPUT").unwrap();
+    let output_file = matches.value_of("OUTPUT").unwrap();
+    convert(&palette, input_file, output_file);
 }
 
-fn convert(palette: &HashMap<String, Color>) {
-    let mut img = image::open("example.png")
+fn convert(palette: &HashMap<String, Color>, input_file: &str, output_file: &str) {
+    let mut img = image::open(input_file)
         .expect("Failed to read input image")
         .to_rgb8();
 
@@ -27,8 +48,7 @@ fn convert(palette: &HashMap<String, Color>) {
         }
         let replacement = Rgb(nearest.rgb);
         img.put_pixel(x, y, replacement);
-        // println!("{:?}", nearest.rgb);
     }
 
-    img.save("output.png").expect("Failed to store output image");
+    img.save(output_file).expect("Failed to store output image");
 }
